4c290e4ac219b702874d9785c687527f87014a83
Dexter <dex429.dc@gmail.com>
2020-12-19 13:07:01 -0500

    commit tree d650672c3a555f2645e44f943a346e106eb3a5fd

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,24 @@
 def oxford_comma(array)
+   #if array.length == 2
+   # "#{array.first} and #{array.last}"
+   #elsif array.length > 2
+   #  something = array.pop
+   #  array << "and #{something}"
+  # else 
+  # end
 
+  case array.length
+  when 1
+    "#{array[0]}"
+  when 2
+    array[0..1].join(" and ")
+  else
+    array[0...-1].join(", ") << ", and #{array[-1]}"
+  end
 end
+
+
+
+
+
+
